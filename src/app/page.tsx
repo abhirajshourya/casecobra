343e224990e84c92e9d31ece0524dc5cceef8d8b
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2024-05-29 19:14:42 -0400

    feat: Add icon component and testimonials
    This commit adds a new file, `Icons.tsx`, which contains a component for rendering an underline icon. The component uses the `LucideProps` type from the `lucide-react` library and renders an SVG with a specific path. This addition enhances the available icons in the project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
+import { Icons } from '@/components/Icons';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Phone from '@/components/Phone';
+import Reviews from '@/components/Reviews';
 import { Check, Star } from 'lucide-react';
 
 export default function Home() {
@@ -93,8 +96,134 @@ export default function Home() {
                 className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
                 alt="your image"
               />
-              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt="underline"
+              />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{' '}
+              <span className="relative px-2">
+                customers{' '}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{' '}
+              say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" alt="logo" />
+          </div>
+
+          {/* User Reviews */}
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            {/* Review 1 */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the design. Had the case
+                  for two and a half months now and{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>,
+                  on the case I had before, the image started to fade after a month. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-1.png"
+                  alt="user image"
+                  className="rounded-full h-12 w-12 object-cover"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Review 2 */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  <p>
+                    "The case arrived quickly and the quality is amazing. The image on the case
+                    looks vibrant and clear. I've been using it for a few weeks now and it{' '}
+                    <span className="p-0.5 bg-slate-800 text-white">still looks brand new</span>.
+                    Highly recommend!"
+                  </p>
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-2.png"
+                  alt="user image"
+                  className="rounded-full h-12 w-12 object-cover"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Alex</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Review 3 */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I've had my case for a few months now and it's still in great condition. The
+                  image is still clear and the case is still in great shape. I've dropped my phone a
+                  few times and the{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">case has protected it well</span>.
+                  I'm very happy with my purchase."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-3.png"
+                  alt="user image"
+                  className="rounded-full h-12 w-12 object-cover"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Molly</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
